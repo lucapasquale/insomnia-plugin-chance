25a76cca381a06fe8852832b004bf4e7b65e4878
Luca Pasquale <lucapasquale.094@gmail.com>
2018-03-05 22:25:35 -0300

    using only chance custom function

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
-# Insomnia Plugin - Random Generator
+# Insomnia Plugin - Chance
+## Random Value Generator
+
+Generates a random value from on [Chance.JS](http://chancejs.com/)
+
+### Args:
+- Function: The name of the Chance function to be called
+
+- Options: Stringified arguments to be used inside chance function
+
+### Example:
 
 ![Screenshot](/readme-preview.png?raw=true)
-
-## Generators:
-### Email
-### String
-### CPF
-### Natural Number
-
--- index.js
+++ index.js
@@ -2,40 +2,23 @@ const Chance = require('chance');
 const chance = new Chance();
 
 module.exports.templateTags = [{
-  name: 'random',
-  displayName: 'Random Value Generator',
-  description: 'Generates a random value',
+  name: 'chance',
+  displayName: 'Chance Value Generator',
+  description: 'Generates a random value using Chance.JS',
   args: [
     {
-      displayName: 'Type',
-      type: 'enum',
-      options: [
-        {
-          displayName: 'Random Email',
-          value: 'email'
-        },
-        {
-          displayName: 'Random String',
-          value: 'string'
-        },
-        {
-          displayName: 'Random CPF',
-          value: 'cpf'
-        },
-        {
-          displayName: 'Random Natural Number',
-          value: 'natural'
-        },
-      ],
+      displayName: 'Chance Function Name',
+      type: 'string',
+      placeholder: 'natural',
+    },
+    {
+      displayName: 'Stringified Options',
+      type: 'string',
+      placeholder: '"min": 10, "max": 25',
     },
   ],
-  async run(context, type = 'email') {
-    switch (type) {
-      case 'email': return chance.email();
-      case 'string': return chance.string();
-      case 'cpf': return chance.cpf();
-      case 'natural': return chance.natural();
-      default: throw new Error('invalid type');
-    }
+  async run(context, func = 'natural', opt = '') {
+    const options = JSON.parse(`{ ${opt} }`);
+    return chance[func](options);
   }
 }];
