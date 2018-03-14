eecb8ec348a64148a517ead53abda0e936badbb1
Luca Pasquale <lucapasquale.094@gmail.com>
2018-03-14 16:08:05 -0300

    changed to enum

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
 const Chance = require('chance');
-
-// const email = require('./tags/email');
-const custom = require('./tags/custom');
-
 const chance = new Chance();
+
 module.exports.templateTags = [
-  // email.default(chance),
-  custom.default(chance),
+  {
+    name: 'chance',
+    displayName: 'Chance',
+    description: 'Generates a random value based on Chance.JS',
+    args: [
+      {
+        displayName: 'Type',
+        defaultValue: 'email',
+        type: 'enum',
+        options: [
+          { displayName: 'Natural', value: 'natural' },
+          { displayName: 'Float', value: 'floating' },
+          { displayName: 'Dice (d6)', value: 'd6' },
+          { displayName: 'Dice (d20)', value: 'd20' },
+          { displayName: 'Name', value: 'name' },
+          { displayName: 'Email', value: 'email' },
+          { displayName: 'Gender', value: 'gender' },
+          { displayName: 'Phone', value: 'phone' },
+          { displayName: 'Zipcode', value: 'zip' },
+          { displayName: 'Credit Card', value: 'cc' },
+          { displayName: 'Date', value: 'date' },
+          { displayName: 'Word', value: 'word' },
+          { displayName: 'CPF', value: 'cpf' },
+          { displayName: 'Custom', value: 'custom' },
+        ],
+      },
+      {
+        displayName: 'Custom Function - Only if selected type is Custom',
+        type: 'string',
+        placeholder: 'integer',
+      },
+      {
+        displayName: 'Options - Stringified arguments for chosen function',
+        type: 'string',
+        placeholder: '"min": 10, "max": 25',
+      },
+    ],
+    async run(context, type, func, opt = '') {
+      const customFunction = type === 'custom' ? func : type;
+      const options = JSON.parse(`{${opt}}`)
+
+      return chance[customFunction](options);
+    }
+  },
 ];
