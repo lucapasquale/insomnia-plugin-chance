22cf9612c550e0e539c68db799daad09f5c75a02
Luca Pasquale <lucapasquale.094@gmail.com>
2018-03-14 16:19:19 -0300

    Version 1.1 (#2)
    * started tags
    
    * changed to enum
    
    * updated readme
    
    * v1.1

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
 const Chance = require('chance');
 const chance = new Chance();
 
-module.exports.templateTags = [{
-  name: 'chance',
-  displayName: 'Chance Value Generator',
-  description: 'Generates a random value using Chance.JS',
-  args: [
-    {
-      displayName: 'Chance Function Name',
-      type: 'string',
-      placeholder: 'natural',
-    },
-    {
-      displayName: 'Stringified Options',
-      type: 'string',
-      placeholder: '"min": 10, "max": 25',
-    },
-  ],
-  async run(context, func = 'natural', opt = '') {
-    const options = JSON.parse(`{ ${opt} }`);
-    return chance[func](options);
-  }
-}];
+module.exports.templateTags = [
+  {
+    name: 'chance',
+    displayName: 'Chance',
+    description: 'Generates a random value based on Chance.JS',
+    args: [
+      {
+        displayName: 'Type',
+        defaultValue: 'email',
+        type: 'enum',
+        options: [
+          { displayName: 'Natural', value: 'natural' },
+          { displayName: 'Float', value: 'floating' },
+          { displayName: 'Dice (d6)', value: 'd6' },
+          { displayName: 'Dice (d20)', value: 'd20' },
+          { displayName: 'Name', value: 'name' },
+          { displayName: 'Email', value: 'email' },
+          { displayName: 'Gender', value: 'gender' },
+          { displayName: 'Phone', value: 'phone' },
+          { displayName: 'Zipcode', value: 'zip' },
+          { displayName: 'Credit Card', value: 'cc' },
+          { displayName: 'Date', value: 'date' },
+          { displayName: 'Word', value: 'word' },
+          { displayName: 'CPF', value: 'cpf' },
+          { displayName: 'Custom', value: 'custom' },
+        ],
+      },
+      {
+        displayName: 'Custom Function - Only if selected type is Custom',
+        type: 'string',
+        placeholder: 'integer',
+      },
+      {
+        displayName: 'Options - Stringified arguments for chosen function',
+        type: 'string',
+        placeholder: '"min": 10, "max": 25',
+      },
+    ],
+    async run(context, type, func, opt = '') {
+      const customFunction = type === 'custom' ? func : type;
+      const options = JSON.parse(`{${opt}}`)
+
+      return chance[customFunction](options);
+    }
+  },
+];
